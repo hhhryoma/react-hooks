effb86b40077d057d95b143853fb347ff61cef12
hhhryoma <poipoi.rh@gmail.com>
2020-12-07 00:04:04 +0900

    useState

--- react-hooks-101/src/App.js
+++ react-hooks-101/src/App.js
@@ -1,33 +1,26 @@
 import React, {Fragment, useState} from 'react'
 
-const App = () => {
-    const [count, setCount] = useState(0)
-    const reset = () => setCount(0)
-    const increment = () => setCount(count + 1)
-    const decrement = () => setCount(count - 1)
-
-    const increment2 = () => setCount(prevCount => prevCount + 1)
-    const decrement2 = () => setCount(prevCount => prevCount - 1)
-    const double = () => setCount(prevCount => prevCount * 2)
-    const devide = () => setCount(prevCount => (prevCount % 3 === 0 ? prevCount/3 : prevCount))
+const App = props => {
+    const [name, setName] = useState(props.name)
+    const [price, setPrice] = useState(props.price)
+    const reset = () => {
+        setPrice(props.price)
+        setName(props.name)
+    }
     return (
         <>
-            <div>count: {count}</div>
-            <div>
-                <button onClick={increment}>+1</button>
-                <button onClick={decrement}>-1</button>
-            </div>
-            <div>
-                <button onClick={increment2}>+1</button>
-                <button onClick={decrement2}>-1</button>
-            </div>
-            <div>
-                <button onClick={reset}>reset</button>
-                <button onClick={double}>double</button>
-                <button onClick={devide}>３の倍数のときだけ３でわる</button>
-            </div>
+            <p>現在の{name}は、{price}円です。</p>
+            <button onClick={() => setPrice(price + 1)}>+1</button>
+            <button onClick={() => setPrice(price - 1)}>-1</button>
+            <button onClick={reset}>Reset</button>
+            <input value={name} onChange={e => setName(e.target.value)}/>
         </>
     )
+}
+
+App.defaultProps = {
+    name: "aaa",
+    price: 1000
 }
 
 export default App
