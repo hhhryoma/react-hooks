b29daf163dc810c996b60811e42333a279b03219
hhhryoma <poipoi.rh@gmail.com>
2020-12-13 15:26:46 +0900

    implement

--- react-hooks-101/src/component/Event.js
+++ react-hooks-101/src/component/Event.js
@@ -3,7 +3,8 @@ import React, {useContext} from 'react'
 const Event = ({dispatch, event}) => {
     const id = event.id
     const handleClickDeleteButton = () => {
-        dispatch({type: 'DELETE_EVENT', id})
+        const result = window.confirm(`イベント{id=${id}}を本当に削除しても良いですか？`)
+        if (result) {dispatch({type: 'DELETE_EVENT'}, id)}
     }
     return (
         <tr>
